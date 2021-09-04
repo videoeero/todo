6e05e31d976eb443313ed20d94e0bbb1dace9752
Eero Suvanto <eero.suvanto@teliacompany.com>
2021-09-04 15:33:15 +0300

    Add Edit todo and disabled mode

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,7 +5,11 @@ import "./TodoList.scss";
 const TodoList = () => {
   const [todoData, setTodoData] = useState([]);
 
+  const [editMode, setEditMode] = useState(false);
+
   const newTodoInput = createRef();
+
+  const [editTodoInput, setEditTodoInput] = useState(null);
 
   useEffect(() => {
     getTodoData();
@@ -48,6 +52,7 @@ const TodoList = () => {
 
   const editTodo = (index) => {
     console.log(todoData[index]);
+    setEditMode(index);
   };
 
   const toggleTodoDone = (item, index) => {
@@ -55,50 +60,91 @@ const TodoList = () => {
 
     const updatedItemStatus = { ...item, status: !item.status };
 
-    const newList = todoData.map((x, i) => {
-      return i == index ? updatedItemStatus : x;
+    const newList = todoData.map((originalItem, i) => {
+      return i === index ? updatedItemStatus : originalItem;
+    });
+    setTodoData(newList);
+  };
+
+  const updateTodoTask = (index, originalTask) => {
+    const task = editTodoInput !== null ? editTodoInput : originalTask;
+
+    const updatedTask = { task, status: false };
+    console.log(updatedTask);
+
+    const newList = todoData.map((originalItem, i) => {
+      return i === index ? updatedTask : originalItem;
     });
 
+    setEditTodoInput(null);
+
+    setEditMode(false);
     setTodoData(newList);
+  };
+
+  const isDisabled = () => {
+    return editMode === false ? false : true;
   };
 
   return (
     <>
       <div>
-        <input ref={newTodoInput} type="text" />
-        <button onClick={addNewTodo}>Add new todo</button>
+        <input ref={newTodoInput} type="text" disabled={isDisabled()} />
+        <button onClick={addNewTodo} disabled={isDisabled()}>
+          Add new todo
+        </button>
       </div>
 
       <ul>
-        {console.log(todoData)}
-        {todoData.map((item, index) => (
-          <li key={index}>
-            <button
-              onClick={() => {
-                toggleTodoDone(item, index);
-              }}
-            >
-              Check
-            </button>
-            <span className={!item.status ? "item_false" : "item_true"}>
-              {item.task}
-            </span>
-            <button
-              onClick={() => {
-                editTodo(index);
-              }}
-            >
-              Edit
-            </button>
-            <button
-              onClick={() => {
-                deleteTodo(index);
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
+        {todoData.map((item, index) =>
+          editMode === index ? (
+            <li key={index}>
+              <input
+                type="text"
+                value={editTodoInput === null ? item.task : editTodoInput}
+                onChange={(e) => setEditTodoInput(e.target.value)}
+              />
+              <button
+                onClick={() => {
+                  updateTodoTask(index, item.task);
+                }}
+              >
+                OK
+              </button>
+              <button>Cancel</button>
+            </li>
+          ) : (
+            <li key={index}>
+              <span
+                onClick={() => {
+                  toggleTodoDone(item, index);
+                }}
+              >
+                <button disabled={isDisabled()}>Check</button>
+                <span className={!item.status ? "item_false" : "item_true"}>
+                  {item.task}
+                </span>
+              </span>
+
+              <button
+                onClick={() => {
+                  editTodo(index);
+                }}
+                disabled={isDisabled() | item.status}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  deleteTodo(index);
+                }}
+                disabled={isDisabled() | !item.status}
+              >
+                Delete
+              </button>
+            </li>
+          )
+        )}
       </ul>
     </>
   );
